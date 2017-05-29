d79fe58d122ef5e3b1e88952fbcabddba85c9687
zaynex <zaynex@126.com>
2017-05-29 23:31:55 +0800

    update reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,28 +4,11 @@ import '../assets/css/styles.css';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+        <h1>Reactive Forms</h1>
+        <hero-detail></hero-detail>
+    </div>
+  `
 })
-export class AppComponent {
-  constructor(
-    private _element: ElementRef
-  ){}
-  
-  public toggleFullScreen() {
-    // 在Ynote中全屏时需要获得iframe的DOM元素，因为不能直接获得，（异步）
-    // 所以可以先放大body，待元素获得完全后再通过zindex去覆盖body
-    let elem = this._element.nativeElement.querySelector('.demo-content');
-    if(elem.requestFullScreen) {
-        elem.requestFullScreen();
-    } else if(elem.webkitRequestFullScreen) {
-        elem.webkitRequestFullScreen();
-    } else if(elem.mozRequestFullScreen) {
-        elem.mozRequestFullScreen();
-    } else if(elem.msRequestFullScreen) {
-        elem.msRequestFullScreen();
-    } else {
-        alert("your broswer doesn't support fullscreen");
-    }
-  }
- }
+export class AppComponent {}
